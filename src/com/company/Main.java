268cf91cb3869876dc0f0f9b5d9e3c935e91ffc8
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-01-17 18:34:12 +0100

    Task 11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,11 +5,13 @@ public class Main {
     public static void main(String[] args) {
         int number = 10;
 
-        while (number > 0){
+       do {
+           System.out.println(number);
+           number--;
+       }
+       while (number > 0);
 
-            System.out.println(number);
-            number--;
-        }
+
 
 
 
