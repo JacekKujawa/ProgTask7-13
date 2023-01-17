ff93d86e8d91ffd1a44fcd3ce494e058e9744909
JacekKujawa <jacek.kujawa0909@gmail.com>
2023-01-17 18:30:22 +0100

    Task 10

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,15 @@
 package com.company;
 
-
-
-
-
 public class Main {
 
     public static void main(String[] args) {
+        int number = 10;
 
+        while (number > 0){
 
-        Student student1 = new Student();
-        student1.NumberIsEvenSwitch(11);
-        student1.NumberIsEven(12);
+            System.out.println(number);
+            number--;
+        }
 
 
 
